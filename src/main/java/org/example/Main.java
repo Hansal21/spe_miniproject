829a700b001df56358011bcf8fa2735ceb59c69d
Hansal21 <hansaldhandha123@gmail.com>
2023-03-09 18:38:04 +0530

    Added sqrt function and Junit test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,13 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter Number:");
-        Double num=sc.nextDouble();
 
-//        System.out.println("1.Sum");
+    public double sqrt(double x){
+        return Math.sqrt(x);
+    }
+    public static void main(String[] args) {
+        Main main=new Main();
+        Scanner sc=new Scanner(System.in);
         System.out.println("1.Square root function - √x");
         System.out.println("2.Factorial function - x!");
         System.out.println("3.Natural logarithm (base е) - ln(x)");
@@ -17,7 +18,9 @@ public class Main {
         int choice=sc.nextInt();
         switch (choice){
             case 1:
-                System.out.println(Math.sqrt(num));
+                System.out.print("Enter Number:");
+                Double num=sc.nextDouble();
+                System.out.println("Square root of "+num+" is "+main.sqrt(num));
                 break;
             case 2:
                 break;
@@ -30,4 +33,6 @@ public class Main {
         }
 
     }
+
+
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,5 +1,19 @@
-
+import org.example.Main;
+import static org.junit.Assert.*;
+import org.junit.*;
 
 public class CalculatorTest {
 
+    Main main=new Main();
+    @Test
+    public void test_sqrt(){
+        assertEquals(3,main.sqrt(9),0);
+        assertEquals(10,main.sqrt(100),0);
+        assertEquals(5,main.sqrt(25),0);
+        assertEquals(1,main.sqrt(1),0);
+        assertNotEquals(23,main.sqrt(9),0);
+        assertNotEquals(120,main.sqrt(100),0);
+        assertNotEquals(52,main.sqrt(25),0);
+        assertNotEquals(12,main.sqrt(1),0);
+        }
 }

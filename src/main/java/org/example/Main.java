c530ca1555a2d3691933dafaa1d80c79b0d62445
Hansal21 <hansaldhandha123@gmail.com>
2023-03-09 21:41:56 +0530

    Added power function and Junit test for it

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,6 +10,9 @@ public class Main {
     public double log(double x){
         return Math.log(x);
     }
+    public double power(Double base,Double power){
+        return Math.pow(base,power);
+    }
     public Integer factorial(Integer num) {
         Integer x=1;
         for(int i=1;i<=num;i++){
@@ -35,14 +38,19 @@ public class Main {
             case 2:
                 System.out.print("Enter Number:");
                 Integer x=sc.nextInt();
-                System.out.println(main.factorial(x));
+                System.out.println("Factorial of "+x+" is "+main.factorial(x));
                 break;
             case 3:
                 System.out.print("Enter Number:");
                 num=sc.nextDouble();
-                System.out.println(main.log(num));
+                System.out.println("Natural Log of "+num+" is "+main.log(num));
                 break;
             case 4:
+                System.out.print("Enter base Number:");
+                Double base=sc.nextDouble();
+                System.out.print("Enter power:");
+                Double power=sc.nextDouble();
+                System.out.println(base+" Power "+power+" is "+main.power(base,power));
                 break;
             default:
 
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -27,7 +27,8 @@ public class CalculatorTest {
         assertNotEquals(2,main.factorial(1),DELTA);
         assertNotEquals(52,main.factorial(5),DELTA);
         assertNotEquals(12,main.factorial(8),DELTA);
-    }  @Test
+    }
+    @Test
     public void test_log(){
         assertEquals(2.30258509299,main.log(10),DELTA);
         assertEquals(3.91202300543,main.log(50),DELTA);
@@ -37,5 +38,15 @@ public class CalculatorTest {
         assertNotEquals(2,main.log(50),DELTA);
         assertNotEquals(52,main.log(13),DELTA);
         assertNotEquals(12,main.log(1),DELTA);
+    }@Test
+    public void test_power(){
+        assertEquals(1,main.power(1.0,0.0),DELTA);
+        assertEquals(0,main.power(0.0,2.0),DELTA);
+        assertEquals(27,main.power(3.0,3.0),DELTA);
+        assertEquals(25 ,main.power(5.0,2.0),DELTA);
+        assertNotEquals(0,main.power(10.0,3.0),DELTA);
+        assertNotEquals(2,main.power(1.0,2.0),DELTA);
+        assertNotEquals(52,main.power(3.0,2.0),DELTA);
+        assertNotEquals(12,main.power(12.0,2.0),DELTA);
     }
 }
